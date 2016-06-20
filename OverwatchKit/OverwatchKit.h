2588732bcfa25675d341b32a6d5aab63a455a1bc
Carlos Grossi <carlos.grossi@gmail.com>
2016-06-20 18:14:40 -0300

    Initial Commit

---
+++ OverwatchKit/OverwatchKit.h
@@ -0,0 +1,19 @@
+//
+//  OverwatchKit.h
+//  OverwatchKit
+//
+//  Created by Carlos Grossi on 20/6/16.
+//  Copyright © 2016 Carlos Grossi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OverwatchKit.
+FOUNDATION_EXPORT double OverwatchKitVersionNumber;
+
+//! Project version string for OverwatchKit.
+FOUNDATION_EXPORT const unsigned char OverwatchKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OverwatchKit/PublicHeader.h>
+
+
